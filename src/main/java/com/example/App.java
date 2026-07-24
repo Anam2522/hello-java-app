2eb3141d8b27e58aa0c4346ec44f3bbcdfb8d341
Anam2522 <ekramullah99@gmail.com>
2026-07-24 13:03:38 +0530

    Initial Hello Java application

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("==================================");
+        System.out.println("Hello World from Jenkins CI/CD");
+        System.out.println("Running on Kubernetes");
+        System.out.println("==================================");
+    }
+}
